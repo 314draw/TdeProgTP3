159cc3141264c5bfca52ca4789df8e8a4950a093
314draw <pedrosecheverria@gmail.com>
2016-09-23 18:30:19 -0300

    Agregando ENcriptador Cesar

--- Ej4/EncriptadorCesar.cs
+++ Ej4/EncriptadorCesar.cs
@@ -2,8 +2,30 @@ using System;
 
 namespace Ej4
 {
-	public class EncriptadorCesar
+	public class EncriptadorCesar:Encriptador
 	{
+		private int iDesplazamiento;
+
+		public int Desplazamiento
+		{
+			get{return this.iDesplazamiento;}
+			set{this.iDesplazamiento = value;}
+		}
+		override string Encriptar (string pCadena)
+		{
+			String cadenaFinal,aux = "";
+			aux = pCadena;
+			for (int i = 0; i <this.Desplazamiento; i++) 
+			{
+				aux [pCadena.Length + i + 1] = pCadena [i];
+			}
+			for (int j=3; j<aux.Length; j++) 
+			{
+				cadenaFinal += aux [j];
+			}
+			return cadenaFinal;
+		}
+
 		public EncriptadorCesar ()
 		{
 		}
