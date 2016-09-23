4c00c87387f606ab6f5f76859e87df92a213dad4
314draw <pedrosecheverria@gmail.com>
2016-09-23 16:19:48 -0300

    ejercicios creado del 1 al 4

---
+++ Ej2/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Ej2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ Ej3/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Ej3
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
